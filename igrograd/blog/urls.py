68fd1aa080daa81ac06a947b0af1bb08a6c763d2
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-06 18:45:23 +0300

    Add new paths urls

---
+++ igrograd/blog/urls.py
@@ -0,0 +1,9 @@
+import views
+from django.urls import path
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail')
+]
