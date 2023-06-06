2941f1f04e77f2a08c3d5ce28f194d802f4cdc25
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-06 16:41:34 +0300

    Add Post and Status class

--- igrograd/blog/models.py
+++ igrograd/blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Status(models.TextChoices):
+    DRAFT = 'OF', 'Черновик'
+    PUBLISHED = 'ON', 'Опубликован'
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Публикация',
+        help_text='Заполните название публикации')
+    slug = models.SlugField(
+        max_length=250,
+        verbose_name='Слаг',
+        help_text='Заполните уникальный фрагмент URL-адреса',
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+        help_text='Заполните текст публикации'
+    )
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    class Meta:
+        ordering = ('-publish',)
+        indexes = [
+            models.Index(fields=('-publish',))
+        ]
+
+    def __str__(self) -> str:
+        return self.title
