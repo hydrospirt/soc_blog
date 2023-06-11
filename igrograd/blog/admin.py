89c486ad231522a65f96a177f6cb62e287545ce6
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-11 13:58:06 +0300

    Add new class CommentAdmin

--- igrograd/blog/admin.py
+++ igrograd/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Comment, Post
 
 
 @admin.register(Post)
@@ -23,3 +23,24 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'post',
+        'created',
+        'active',
+    )
+    list_filter = (
+        'active',
+        'created',
+        'updated',
+    )
+    search_fields = (
+        'name',
+        'email',
+        'text',
+    )
