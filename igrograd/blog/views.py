33c6733493acf5abc161e5cf226c996c58488680
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-13 12:35:47 +0300

    Add post_list

--- igrograd/blog/views.py
+++ igrograd/blog/views.py
@@ -1,35 +1,30 @@
 from decouple import config
 from django.core.mail import send_mail
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
+from taggit.models import Tag
 
 from .forms import CommentForm, EmailPostForm
 from .models import Post, Status
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-
-#     return render(
-#         request,
-#         'blog/post/list.html',
-#         {'posts': posts}
-#     )
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
 
 
 def post_detail(request, year, month, day, post):
