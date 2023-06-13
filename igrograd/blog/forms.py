32388065fa58a71e5095ddca51d58b49743e92da
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-13 13:02:05 +0300

    Add label for form

--- igrograd/blog/forms.py
+++ igrograd/blog/forms.py
@@ -6,12 +6,18 @@ from .models import Comment
 class EmailPostForm(forms.Form):
     name = forms.CharField(
         max_length=25,
+        label='Ваше имя',
     )
-    email = forms.EmailField()
-    to = forms.EmailField()
+    email = forms.EmailField(
+        label='Ваш email'
+    )
+    to = forms.EmailField(
+        label='Email получателя'
+    )
     comments = forms.CharField(
         required=False,
         widget=forms.Textarea,
+        label='Комментарий для получателя'
     )
 
 
