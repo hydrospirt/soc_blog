0a723a2b4a4713c241d25a7ca9800835d80bd88b
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-14 18:31:37 +0300

    Add SearchQuery and SearchRank

--- igrograd/blog/views.py
+++ igrograd/blog/views.py
@@ -1,5 +1,6 @@
 from decouple import config
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (SearchQuery, SearchRank,
+                                            SearchVector)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -128,9 +129,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'text')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'text'),
-                ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+                ).filter(search=search_query).order_by('-rank')
     return render(
         request,
         'blog/post/search.html',
